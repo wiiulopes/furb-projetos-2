8286a985f29bdc2095c55d52015ace6a186b7e5e
William Lopes da Silva <william.silva@hbsis.com.br>
2018-09-27 21:09:27 -0300

    3 - Ajuste consulta cde busca no banco de dados

--- main/busca.py
+++ main/busca.py
@@ -1,13 +1,49 @@
-import pymysql
-conn= pymysql.connect(host='localhost',port=3306, user='root',password='Wiiu12345*',db='vision')
-cur = conn.cursor()
-cur.execute("SELECT * FROM coordenada")
+from pymysql import connect, Error
 
-print(cur.description)
-print()
 
-for row in cur:
-    print(row)
+#Busca todos pontos de interesse
+def select_all_coordenada():
+    try:
+        conn = connect(host='localhost', port=3306, user='root', password='Wiiu12345*', db='vision')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM coordenada")
 
-cur.close()
-conn.close()
+        row = cursor.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cursor.fetchone()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+#Busca pontos de interesse conforme filtro
+def select_lat_lng():
+    try:
+        conn = connect(host='localhost', port=3306, user='root', password='Wiiu12345*', db='vision')
+        cursor = conn.cursor()
+        q = "SELECT * FROM coordenada where lat = %(value)s and lng = %(value2)s"
+        params = {'value': '-26.91534000', 'value2': '-49.08198400'}
+        cursor.execute(q, params)
+
+        row = cursor.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cursor.fetchone()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__ == '__main__':
+    select_all_coordenada()
+    select_lat_lng()
